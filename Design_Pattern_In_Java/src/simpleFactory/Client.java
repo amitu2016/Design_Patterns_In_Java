8f2de2f1efe536c65c3fa88a95b696df7e856f92
Amit Upadhyay <amitu@cdac.in>
2022-12-14 17:50:34 +0530

    simple factory pattern

---
+++ Design_Pattern_In_Java/src/simpleFactory/Client.java
@@ -0,0 +1,13 @@
+package simpleFactory;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		Post post = PostFactory.createPost("product");
+		
+		System.out.println(post);
+
+	}
+
+}
---
+++ Design_Pattern_In_Java/src/simpleFactory/PostFactory.java
@@ -0,0 +1,26 @@
+package simpleFactory;
+
+/**
+ * This class acts as a simple factory for creation of different posts on web
+ * site.
+ *
+ */
+public class PostFactory {
+
+	public static Post createPost(String type) {
+
+		switch (type) {
+
+		case "blog":
+			return new BlogPost();
+		case "news":
+			return new NewsPost();
+		case "product":
+			return new ProductPost();
+
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + type);
+		}
+
+	}
+}
