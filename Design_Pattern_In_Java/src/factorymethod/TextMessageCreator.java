5c015e8dcc17a0d9100f7e24c75e15bf52bda2d9
Amit Upadhyay <amitu@cdac.in>
2022-12-15 17:38:01 +0530

    factorymethod

---
+++ Design_Pattern_In_Java/src/factorymethod/TextMessageCreator.java
@@ -0,0 +1,18 @@
+package factorymethod;
+
+import factorymethod.message.Message;
+import factorymethod.message.TextMessage;
+
+/**
+ * Provides implementation for creating Text messages
+ */
+public class TextMessageCreator extends MessageCreator {
+
+	@Override
+	public Message createMessage() {
+		return new TextMessage();
+	}
+
+
+
+}
